35ff39c2cca860b0883e4f75f5b5859d752a9278
Philip Keller <philipjoelkeller@gmail.com>
2023-03-14 14:47:07 -0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Philip Keller on 3/14/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
